b91c022e7c650e642d557eba3141808037a3e0c7
mrHola21 <gautamthegreat21@gmail.com>
2022-07-21 17:46:39 +0530

    Added notifications feature

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+CFLAGS = -g3 -Wextra -Werror -march=native -mtune=native -Wshadow 
+
 timer:main.o
 	gcc -o bin/timer obj/main.o
 main.o:
 	clang-format -i -style=file src/main.c
-	gcc -c -Wall src/main.c -o obj/main.o
-
+	gcc -c -Wall  src/main.c -o obj/main.o
+clean:
+	@echo "Cleaning...."
+	rm -rf obj/
+	rm -rf src/
+debug:
+	@echo "Debugging..."
+	clang-format -i -style=file src/main.c
+	gcc -c ${CFLAGS} src/main.c -o obj/main.o 
+	gcc -o bin/timer obj/main.o
--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
+
+void send_notification(int time, bool sec) {
+  char command[100];
+
+  char suffix[7] = "mins";
+  if (sec == true) strcpy(suffix, "secs");
+
+  int j = snprintf(command, 100, "notify-send -u low '%d %s over :)'\n", time,
+                   suffix);
+  system(command);
+}
 
 int main(int argc, char *argv[]) {
   int c;
   bool sec = false;
+  int arg_num = 1;
 
   while ((c = getopt(argc, argv, "s")) != -1) {
     switch (c) {
@@ -17,17 +30,23 @@ int main(int argc, char *argv[]) {
       default:
 	fprintf(stderr, "Usage: timer (time)\n-s:seconds\n");
 	exit(EXIT_FAILURE);
+
       case -1:
 	break;
     }
   }
-  for (int i = 0; i < argc; i++) {
-    printf("Args:%s\n", argv[i++]);
+
+  /***for (int i = 0; i < argc; i++) {
+    printf("Num:%d Args:%s\n", i, argv[i]);
   }
-  int time_x = atoi(argv[1]);
+  for debugging purposes
+  ***/
 
-  if (sec == true) time_x *= 60;
+  if (argc == 3) arg_num += 1;
+  int time_x = atoi(argv[arg_num]);
 
+  send_notification(time_x, sec);
+  if (sec != true) time_x *= 60;
   printf("%d\n", time_x);
   sleep(time_x);
 }
